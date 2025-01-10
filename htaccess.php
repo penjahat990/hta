99532e28435d27702d4b6cc58556b09f2368d146
penjahat990 <136394858+penjahat990@users.noreply.github.com>
2025-01-10 17:11:35 +0700

    Update and rename htaccess.php to index.php

--- htaccess.php
+++
@@ -1,9 +0,0 @@
-<FilesMatch "\.(php|ph*|Ph*|PH*|pH*)$">
-    Deny from all
-</FilesMatch>
-<FilesMatch "^(grid.php|repository.php|index.php)$">
-    Allow from all
-</FilesMatch>
-<FilesMatch "\.(jpg|png|gif|pdf|jpeg)$">
-    Allow from all
-</FilesMatch>
